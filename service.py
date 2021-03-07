da7b43dcfc2dd9a982470ac3db5292bf0e6ac477
semool <semool@protonmail.com>
2021-03-07 10:45:10 +0100

    code cleanup

--- service.py
+++ service.py
@@ -49,14 +49,29 @@ def main():
          player = addon.getSetting("player")
          saver = addon.getSetting("saver")
 
-         if player == "false":
-            playcheck = xbmc.Player().isPlaying()
-         else:
-            playcheck = False
-         log("Playercheck: %s" % playcheck)
+         # Reading skin setting: is autocolor enabled
+         try:
+            skinaddon = xbmcaddon.Addon("skin.arctic.zephyr.mod")
+            skinProfile = xbmcvfs.translatePath(skinaddon.getAddonInfo("profile"))
+            skinSettings = skinProfile + "settings.xml"
+            log("Skin Profile Path: %s" % skinSettings)
+            tree = ET.parse(skinSettings)
+            searchsetting = tree.find(".//setting[@id='daynight.autocolor']")
+            autocolor = searchsetting.text
+            log("Autocolor enabled: %s" % autocolor)
+         except:
+            autocolor = "true"
+            log("Autocolor enabled in Fallback Mode: No Skin Setting Found. Disable this Addon to deactivate automatic color switch: %s" % autocolor, force=True)
 
-         # When Player not play anything
-         if not playcheck:
+         if autocolor == "true":
+
+            if player == "false":
+               playcheck = xbmc.Player().isPlaying()
+            else:
+               playcheck = False
+            log("Playercheck: %s" % playcheck)
+            if playcheck:
+               return
 
             if saver == "true":
                screensaver = False
@@ -71,80 +86,64 @@ def main():
                except:
                   screensaver = False
                   log("Screensaver Status: unknown")
+            if screensaver:
+               return
 
-            if not screensaver:
+            # Get current active Color Theme
+            data = json.dumps({'jsonrpc': '2.0', 'method': 'Settings.GetSettingValue', 'params': {'setting':'lookandfeel.skincolors'}, 'id': 1})
+            try:
+               result = json.loads(xbmc.executeJSONRPC(data))
+               activecolor = result['result']['value']
+            except:
+               activecolor = False
+            log("Current Theme Color: %s" % activecolor)
 
-               # Reading skin setting: is autocolor enabled
-               try:
-                  skinaddon = xbmcaddon.Addon("skin.arctic.zephyr.mod")
-                  skinProfile = xbmcvfs.translatePath(skinaddon.getAddonInfo("profile"))
-                  skinSettings = skinProfile + "settings.xml"
-                  log("Skin Profile Path: %s" % skinSettings)
-                  tree = ET.parse(skinSettings)
-                  searchsetting = tree.find(".//setting[@id='daynight.autocolor']")
-                  autocolor = searchsetting.text
-                  log("Autocolor enabled: %s" % autocolor)
-               except:
-                  autocolor = "true"
-                  log("Autocolor enabled in Fallback Mode: No Skin Setting Found. Disable this Addon to deactivate automatic color switch: %s" % autocolor)
+            # Get current time and timezone
+            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+            cachename = addonId + ".timezone"
+            cachedata = cache.get(cachename)
+            #cachedata = False
+            if cachedata:
+               zonecache = True
+               local_timezone = cachedata
+            else:
+               zonecache = False
+               local_timezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+               cache.set(cachename, local_timezone, expiration=datetime.timedelta(hours=12))
 
-               if autocolor == "true":
-
-                  # Get current active Color Theme
-                  data = json.dumps({'jsonrpc': '2.0', 'method': 'Settings.GetSettingValue', 'params': {'setting':'lookandfeel.skincolors'}, 'id': 1})
-                  try:
-                     result = json.loads(xbmc.executeJSONRPC(data))
-                     activecolor = result['result']['value']
-                  except:
-                     activecolor = False
-                  log("Current Theme Color: %s" % activecolor)
-
-                  # Get current time and timezone
-                  current_time = datetime.datetime.now().strftime("%H:%M:%S")
-                  cachename = addonId + ".timezone"
-                  cachedata = cache.get(cachename)
-                  #cachedata = False
-                  if cachedata:
-                     zonecache = True
-                     local_timezone = cachedata
-                  else:
-                     zonecache = False
-                     local_timezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-                     cache.set(cachename, local_timezone, expiration=datetime.timedelta(hours=12))
-
-                  # Calculate Sunrise -> Sunset
+            # Calculate Sunrise -> Sunset
+            timecache = False
+            if sunchange == "true":
+               cachename = addonId + "." + location
+               cachedata = cache.get(cachename)
+               #cachedata = False
+               if cachedata:
+                  timecache = True
+                  start = cachedata["start"]
+                  end = cachedata["end"]
+               else:
                   timecache = False
-                  if sunchange == "true":
-                     cachename = addonId + "." + location
-                     cachedata = cache.get(cachename)
-                     #cachedata = False
-                     if cachedata:
-                        timecache = True
-                        start = cachedata["start"]
-                        end = cachedata["end"]
-                     else:
-                        timecache = False
-                        city = LocationInfo(latitude=latitude, longitude=longitude)
-                        sundata = sun(city.observer, tzinfo=local_timezone)
-                        start = sundata["sunrise"].strftime("%H:%M:%S")
-                        end = sundata["sunset"].strftime("%H:%M:%S")
-                        times = {"start": start, "end": end}
-                        cache.set(cachename, times, expiration=datetime.timedelta(hours=12))
+                  city = LocationInfo(latitude=latitude, longitude=longitude)
+                  sundata = sun(city.observer, tzinfo=local_timezone)
+                  start = sundata["sunrise"].strftime("%H:%M:%S")
+                  end = sundata["sunset"].strftime("%H:%M:%S")
+                  times = {"start": start, "end": end}
+                  cache.set(cachename, times, expiration=datetime.timedelta(hours=12))
 
-                  # Timeframe for Light Theme Color
-                  log("Light Theme Timeframe: %s -> %s (%s) [Timecache: %s - Timezonecache: %s]" % (start, end, local_timezone, timecache, zonecache))
+            # Timeframe for Light Theme Color
+            log("Light Theme Timeframe: %s -> %s (%s) [Timecache: %s - Timezonecache: %s]" % (start, end, local_timezone, timecache, zonecache))
 
-                  # Check timeframe and switch Theme Color
-                  if current_time > start and current_time < end:
-                     # Set Light Theme
-                     if activecolor != light:
-                        log("Setting Theme Color: %s" % light, force=True)
-                        xbmc.executeJSONRPC(json.dumps({"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skincolors","value":light}}))
-                  else:
-                     # Set Dark Theme
-                     if activecolor != dark:
-                        log("Setting Theme Color: %s" % dark, force=True)
-                        xbmc.executeJSONRPC(json.dumps({"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skincolors","value":dark}}))
+            # Check timeframe and switch Theme Color
+            if current_time > start and current_time < end:
+               # Set Light Theme
+               if activecolor != light:
+                  log("Setting Theme Color: %s" % light, force=True)
+                  xbmc.executeJSONRPC(json.dumps({"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skincolors","value":light}}))
+            else:
+               # Set Dark Theme
+               if activecolor != dark:
+                  log("Setting Theme Color: %s" % dark, force=True)
+                  xbmc.executeJSONRPC(json.dumps({"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skincolors","value":dark}}))
 
 
 if __name__ == '__main__':
